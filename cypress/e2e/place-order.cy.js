54b2990c899c3cd3591a01fcd20ab6174ceab65f
NaMax66 <max.naidovich@gmail.com>
2024-01-08 16:06:31 +0100

    add checkout flow

--- cypress/e2e/place-order.cy.js
+++ cypress/e2e/place-order.cy.js
@@ -1,9 +1,11 @@
 import ProductPage from "../support/page_objects/ProductPage/ProductPage";
 import CartPage from "../support/page_objects/CartPage/CartPage";
+import {AnonymousCheckout} from "../support/page_objects/CheckoutFlow/CheckoutFlow";
 
 describe('place order', () => {
   const productPage = new ProductPage();
   const cartPage = new CartPage();
+  const checkoutFlow = new AnonymousCheckout()
 
   beforeEach(() => {
     cy.log('Clearing cookies and local storage');
@@ -15,13 +17,17 @@ describe('place order', () => {
     cy.log('Adding to cart');
     productPage.visit('snacks/chips/lays-chips-paprika-box-20x40gr');
     productPage.purchase();
-    cy.wait(2000);
+    cy.wait(1000);
     cartPage.visit();
     cartPage.checkout();
 
-    cy.url().should('include', '/shipping');
+    checkoutFlow.fillShippingAddress();
+    checkoutFlow.selectDelivery('Fixed Rate (Ground)');
+    checkoutFlow.proceedToBilling();
+    checkoutFlow.selectPaymentMethod('Manual');
+    checkoutFlow.reviewOrder();
+    checkoutFlow.placeOrder();
 
-    cartPage.visit();
-    cartPage.clearCart();
+    checkoutFlow.isCompleted();
   });
 });
---
+++ cypress/support/page_objects/CheckoutFlow/CheckoutFlowLocators/CheckoutFlowLocators.js
@@ -0,0 +1,6 @@
+export const CheckoutFlowLocators = {
+  SELECT_SHIPPING_ADDRESS_BUTTON: 'button[class="vc-address-selection__link"]',
+  SELECT_COUNTRY_INPUT: 'input[placeholder="Select your country"]',
+  PROCEED_TO_BILLING: 'a[href="/checkout/billing"]',
+  REVIEW_ORDER: 'a[href="/checkout/review"]'
+};
