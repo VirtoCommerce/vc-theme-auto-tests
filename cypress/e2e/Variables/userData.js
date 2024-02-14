9b546afb87fe549e2c8d6a86a82ecb479ed70f7b
ElenaMutykova <elena.mutykova@virtoway.com>
2024-02-14 17:32:44 +0100

    update user data

---
+++ cypress/e2e/Variables/userData.js
@@ -0,0 +1,181 @@
+export default{
+
+userData:[
+
+  {  
+    id: 1,
+    first_name: "Mila",
+    last_name: "Müller",
+    email: "kugeucrayoija-5761@yopmail.com",
+    gender: "Female",
+    password: "Password1",
+    confirm_password: "Password1" ,
+    user_name: "Mila Müller" 
+},
+  {
+    id: 2,
+    first_name: "Andrew",
+    last_name: "Cook",
+    email: "ricreyacrouyi-3425@yopmail.com",
+    gender: "Male",
+    password: "Password1",
+    confirm_password: "Password1",
+    company_name: "Andrew Corporate Kft.",
+    user_name: "Andrew Cook" 
+  },
+  {
+    id: 3,
+    first_name: "Sherill",
+    last_name: "Swanborrow",
+    email: "sswanborrow2@nationalgeographic.com",
+    gender: "Female",
+    password: "lMfd8Jt",
+    confirm_password: "lMfd8Jt"
+  },
+  {
+    id: 4,
+    first_name: "Simona",
+    last_name: "Thring",
+    email: "sthring3@a8.net",
+    gender: "Female",
+    password: "0B1r1pI",
+    confirm_password: "0B1r1pI"
+  },
+  {
+    id: 5,
+    first_name: "Bartholemy",
+    last_name: "Osselton",
+    email: "bosselton4@hubpages.com",
+    gender: "Male",
+    password: "3PYaY1",
+    confirm_password: "3PYaY2"
+  },
+  {
+    id: 6,
+    first_name: "Tripp",
+    last_name: "Bigrigg",
+    email: "tbigrigg5@drupal.org",
+    gender: "Male",
+    password: "IycpEnadmm",
+    confirm_password: "IycpEnadmm"
+  },
+  {
+    id: 7,
+    first_name: "Ross",
+    last_name: "Bagott",
+    email: "rbagott6@msn.com",
+    gender: "Male",
+    password: "NnY7w9aMg",
+    confirm_password: "NnY7w9aMg"
+  },
+  {
+    id: 8,
+    first_name: "Alexia",
+    last_name: "Lince",
+    email: "alince7@hugedomains.com",
+    gender: "Female",
+    password: "B5Ijp6o",
+    confirm_password: "B5Ijp6o"
+  },
+  {
+    id: 9,
+    first_name: "Yorker",
+    last_name: "Kiffe",
+    email: "ykiffe8@imageshack.us",
+    gender: "Genderfluid",
+    password: "tOYhbcY",
+    confirm_password: "tOYhbcY"
+  },
+  {
+    id: 10,
+    first_name: "Sylas",
+    last_name: "Crickmore",
+    email: "scrickmore9@yellowbook.com",
+    gender: "Male",
+    password: "uyek7BUT",
+    confirm_password: "uyek7BUT"
+  },
+  {
+    id: 11,
+    first_name: "Marthena",
+    last_name: "Ginnally",
+    email: "mginnallya@cdbaby.com",
+    gender: "Female",
+    password: "dS5xmFZk",
+    confirm_password: "dS5xmFZk"
+  },
+  {
+    id: 12,
+    first_name: "Shir",
+    last_name: "Brandoni",
+    email: "sbrandonib@csmonitor.com",
+    gender: "Female",
+    password: "25UvIeV",
+    confirm_password: "25UvIeV"
+  },
+  {
+    id: 13,
+    first_name: "Wells",
+    last_name: "Hursey",
+    email: "whurseyc@ustream.tv",
+    gender: "Male",
+    password: "eo0ygmqgYc",
+    confirm_password: "eo0ygmqgYc"
+  },
+  {
+    id: 14,
+    first_name: "Zacharia",
+    last_name: "Garrould",
+    email: "zgarrouldd@usa.gov",
+    gender: "Male",
+    password: "370KcO",
+    confirm_password: "370KcO"
+  },
+  {
+    id: 15,
+    first_name: "Drona",
+    last_name: "Kinsett",
+    email: "dkinsette@amazon.de",
+    gender: "Female",
+    password: "76aSZIp3vw6u",
+    confirm_password: "76aSZIp3vw6u"
+  },
+  {
+    id: 16,
+    first_name: "Eunice",
+    last_name: "Camelin",
+    email: "ecamelinf@youtube.com",
+    gender: "Female",
+    password: "0Sy55ffLSY",
+    confirm_password: "0Sy55ffLSY"
+  },
+  {
+    id: 17,
+    first_name: "Loleta",
+    last_name: "Fearnsides",
+    email: "lfearnsidesg@ask.com",
+    gender: "Female",
+    password: "zdNHfouCfZf",
+    confirm_password: "zdNHfouCfZf"
+  },
+  {
+    id: 18,
+    first_name: "Pace",
+    last_name: "Paulmann",
+    email: "ppaulmannh@sciencedaily.com",
+    gender: "Male",
+    password: "bMIUj8",
+    confirm_password: "bMIUj9"
+  },
+  {
+    id: 19,
+    first_name: "Clair",
+    last_name: "Finby",
+    email: "cfinbyi@gmpg.org",
+    gender: "Female",
+    password: "l7Qw5nP9rDbZ",
+    confirm_password: "l7Qw5nP9rDbZ"
+  }
+
+],
+}
