f1e81e6953211ed650db7619e7a514294679e917
ElenaMutykova <elena.mutykova@virtoway.com>
2024-03-05 19:33:33 +0100

    fix

--- cypress/support/page_objects/Account/Lists/ListsPage.js
+++ cypress/support/page_objects/Account/Lists/ListsPage.js
@@ -1,4 +1,6 @@
-import { ListsLocators } from "../ListsLocators";{}
+import { ListsLocators } from "../ListsLocators";
+//import Lists_data from "./Lists_data";
+
 
 class Lists{
 
@@ -21,25 +23,44 @@ cy.get('.mx-5 > .vc-button').contains('Create list');
 
 }
 
-createPersonalList(){
+createPersonalList(list_name, list_description){
 
 cy.log('Create a new list')
 cy.contains('button', 'Create list').click();
 cy.get('h3').should('be.visible').and('have.text', 'New List');
-cy.get('input[type="text"]').eq(1).type('Cypress test name');
-cy.get('textarea').type('Personal list');
+cy.get('input[type="text"]').eq(1).type(list_name);
+cy.get('textarea').type(list_description);
 cy.get('.flex-wrap > .vc-button--color--primary').should('have.text', 'Create list').click();
 
 cy.log('Check created list')
-cy.contains('a', 'Cypress test name');
+cy.contains('a', list_name);
 
 
 }
 
-emptyListDetailPage(){
+emptyListDetailPage(list_name){
 
-cy.contains('a', "Cypress test name").click();
+cy.contains('a', list_name).click();
+cy.get('h2').should('have.text', list_name);
+cy.get('.text-xl').should('have.text', 'Your list is empty');
+cy.contains('a', 'Continue browsing');
 
+}
+
+compareListsNames(){
+
+cy.log('Compare list name in the left menue and current list title')
+cy.get('a[aria-current="page"]').last().invoke('text').then((text1) => {
+cy.get('.truncate').invoke('text').then((text2) => {
+expect(text1).to.equal(text2)
+cy.log('The lists title are equal');
+});
+});
+
+}
+
+clickToListsRouter(){
+cy.get(ListsLocators.ROUTER_LINK).click();
 }
 
 }
