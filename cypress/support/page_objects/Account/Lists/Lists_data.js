498ca5511c7f36395a2865c91f6db8f3e0d0cda5
ElenaMutykova <elena.mutykova@virtoway.com>
2024-03-05 19:33:24 +0100

    add file with lists names

---
+++ cypress/support/page_objects/Account/Lists/Lists_data.js
@@ -0,0 +1,16 @@
+export default {
+
+lists:[
+{
+name1: 'Cypress_test1',
+description1: 'Cypress_test1. My first personal list'
+},
+
+{
+name2: 'Cypress_test2',
+description2: 'Cypress_test2. My second personal list'
+}
+
+]
+
+}
