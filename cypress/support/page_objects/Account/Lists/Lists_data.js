8b8180e19d66876bd9c916c5c56809443f5375d9
ElenaMutykova <elena.mutykova@virtoway.com>
2024-03-06 15:42:42 +0100

    randomData

--- cypress/support/page_objects/Account/Lists/Lists_data.js
+++ cypress/support/page_objects/Account/Lists/Lists_data.js
@@ -2,7 +2,7 @@ export default {
 
 lists:[
 {
-name1: 'Cypress_test1',
+name1: 'Cypress_test',
 description1: 'Cypress_test1. My first personal list'
 },
 
@@ -11,6 +11,28 @@ name2: 'Cypress_test2',
 description2: 'Cypress_test2. My second personal list'
 }
 
-]
+],
 
+getRandomWord() {
+const words =  [
+    "New Year's Day",
+    "Valentine's Day",
+    "Easter Sunday",
+    "Independence Day",
+    "Halloween",
+    "Thanksgiving Day",
+    "Christmas Day",
+    "Labor Day",
+    "Memorial Day",
+    "Veterans Day",
+    "Martin Luther King",
+    "Presidents' Day",
+    "Good Friday",
+    "Hanukkah",
+    "Diwali"
+];
+
+const randomIndex = Math.floor(Math.random() * words.length);
+return words[randomIndex];
+}
 }
