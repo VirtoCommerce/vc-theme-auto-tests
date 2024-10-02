c133c5f0a7c1ea1c8d4801fc97674b6092f1be7c
ElenaMutykova <elena.mutykova@virtoway.com>
2024-10-02 16:22:00 +0200

    Add @shopify/cypress-graphql package

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,6 +16,7 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import 'cypress-if'
+import '@shopify/cypress-graphql'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
